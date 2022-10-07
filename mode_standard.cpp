eace36577168e71a16900c30275fb81546f2843f
EikyouPH <91479888+EikyouPH@users.noreply.github.com>
2022-10-07 19:42:11 +0200

    Ajout de la librarie pour la carte SD, et début de l'intialisation du moode standard.

--- mode_standard.cpp
+++ mode_standard.cpp
@@ -1,8 +1,19 @@
+#include <SPI.h>
+#include <SD.h>
 
 void setup() {
-  
+  Serial.begin(9600);
+  Serial.println("Demarrage");
+  const int delaiMesure = 1000;
+  const int pinCS = 10;
+  SD.begin(pinCS);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  delay(10000);
 }
 
 void loop() {
-  
+  Serial.println("Mode standard");
+  delay(1000);
+  mesureCapteurs();
 }
