de0aa1e77cf88ab8e66721f15b501d55e3d8ab9d
EikyouPH <91479888+EikyouPH@users.noreply.github.com>
2022-10-12 14:35:14 +0200

    Gestion des leds

--- main.cpp
+++ main.cpp
@@ -7,17 +7,31 @@
 const int delaiMesure = 10000;
 const int delaiMesureEco = 20000;
 const int pinCS = 10;
+const int BoutonRouge = 2;
+const int BoutonVert = 3;
+const int ledOrange = 4;
+const int ledVert = 5;
+const int ledBleue = 6;
+const int ledJaune = 7;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Demarrage Standard");
+  Serial.println("Demarrage du programme");
   SD.begin(pinCS);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
+  pinMode(ledVert, OUTPUT);
+  pinMode(ledBleue, OUTPUT);
+  pinMode(ledJaune, OUTPUT);
+  pinMode(ledOrange, OUTPUT);
+  pinMode(BoutonRouge, INPUT);
+  pinMode(BoutonVert, INPUT);
 }
 
 void loop() {
   Serial.println("Mode standard");
+    digitalWrite(ledBleue, LOW);
+    digitalWrite(ledOrange, LOW);
+    digitalWrite(ledVert, HIGH);
+    digitalWrite(ledJaune, LOW);
     delay(1000);
     void Standard(){
       mesureCapteurs();
@@ -38,6 +52,10 @@ void loop() {
 }
 
 void Eco() {
+    digitalWrite(ledBleue, HIGH);
+    digitalWrite(ledOrange, LOW);
+    digitalWrite(ledVert, lOW);
+    digitalWrite(ledJaune, LOW);
     Serial.println("Mode Eco");
     delay(1000);
     void modeEco(){
@@ -59,14 +77,20 @@ void Eco() {
 }
 
 void Maintenance() {
+  digitalWrite(ledBleue, LOW);
+  digitalWrite(ledOrange, HIGH);
+  digitalWrite(ledVert, lOW);
+  digitalWrite(ledJaune, LOW);
   Serial.println("Mode Maintenance");
   delay(1000);
   void Maintenance(){
-    checkErreur();
-    if (BoutonVert == HIGH){
+    stopMesure();
+    accesSD();
+    affSerie();
+    if (BoutonRouge == HIGH){
       Eco();
     }
-    else if (BoutonRouge == HIGH){
+    else if (BoutonVert == HIGH){
       Standard();
     }
     else {
