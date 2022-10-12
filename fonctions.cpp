b59398c16a7266fe5ebeafaaa7c508b1a4a84560
alanis51 <115553382+alanis51@users.noreply.github.com>
2022-10-12 15:28:53 +0200

    Commentaires

--- fonctions.cpp
+++ fonctions.cpp
@@ -1,15 +1,22 @@
+// Programme contenant les differentes focntions annexes
+// Ces fonctions sont utiles à l'exécution du fichier main.cpp qui contient le programe principale
+
 #include <stdlib.h>
 #include <SoftwareSerial.h>
 
 
-
-//Arrondi un float à 2 chiffres après la virgule
-float arround2(float var)
+// Renvoie un flottant envoyé en entrée arrondi au centieme
+float arrondi(float var)
 {
   float value = (int)(var * 100 + .5);
   return (float)value / 100;
 }
 
+/*
+Toutes les fonctions suivantes permettent de récupérer les données des cpateurs
+Chaque fonction attend en paramètre le numero de pin des capteurs
+Elles renvoient ensuite la valeur captee de type float (a l'exception de la fonction GPS)
+*/
 byte GPS(pinGPS)
 {
   while (ss.available() > 0){
@@ -20,35 +27,36 @@ byte GPS(pinGPS)
 
 float Heure()
 {
-    float heure = round2(analogRead(pinHeure));
+    float heure = arrondi(analogRead(pinHeure));
     return heure;
 }
 
 float mesureLumière(pinLux)
 {
-    float lux = round2(analogRead(pinLux));
+    float lux = arrondi(analogRead(pinLux));
     return lux;
 }
+
 float mesureAir(pinAir)
 {
-    float air = round2(analogRead(pinAir));
+    float air = arrondi(analogRead(pinAir));
     return air;
 }
 
 float mesureTempAir(pinTempAir)
 {
-    float tempAir = round2(analogRead(pinTempAir));
+    float tempAir = arrondi(analogRead(pinTempAir));
     return tempAir;
 }
 
 float mesureTempEau(pinTempEau)
 {
-    float tempEau = round2(analogRead(pinTempEau));
+    float tempEau = arrondi(analogRead(pinTempEau));
     return tempEau;
 }
 
 float mesureHygro(pinHygro)
 {
-    float hygro = round2(analogRead(pinHygro));
+    float hygro = arrondi(analogRead(pinHygro));
     return hygro;
 }
