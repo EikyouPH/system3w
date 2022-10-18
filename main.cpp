3ddd1a29d5d2b589ca088691606032399d04845b
alanis51 <115553382+alanis51@users.noreply.github.com>
2022-10-18 15:03:35 +0200

    Choix initial de mode standard ou config

--- main.cpp
+++ main.cpp
@@ -1,163 +1,38 @@
-// Programme principal
-// Il est le premier lancé et contient toutes les fonctions principales
+enum mode {Standard = 0, Eco, Maintenance, Config};
+const int boutonRouge = 2;
+const int boutonVert = 3;
+bool bRouge = true;
+bool bVert = true;
+mode Mode;
 
-// Importation d'une bibliothèque permettant d'interagir avec des périphriques SPI
-#include <SPI.h>
-// Importation d'une bibliothèque permettant l'écriture sur la carte SD
-#include <SD.h>
-// Importation de l'ensemble de nos fonctions secondaires dans le fichier fonctions.cpp
-#include <fonctions.cpp>
-// Importation de la librairie SoftwareSerial
-#include <SoftwareSerial.h>
-// Importation de la librairie RTC
-#include <iarduino_RTC.h>
-// Importation de la bibliothèque poru l'arrondi
-#include <iostream>
-#include <math.h>
-
-// Initialisation du terminal série ss pour le GPS
-SoftwareSerial ss(4, 3);
-
-// Définition des variables globales
-// Délai entre deux mesures en mode standard (à doubler dans le mode éco)
-const int delaiMesure = 10000;
-// Pin pour lier la carte SD
-const int pinCS = 11;
-// Pins pour les boutons
-const int boutonRouge = 5;
-const int boutonVert = 6;
-// Pins pour les LEDs
-const int pinRouge = 9;
-const int pinVert = 10;
-const int pinBleu = 11;
-// Pins pour les capteurs
-const int pinLux = A1;
-enum Mode {Standard = 0, Eco, Maintenance, Config};
-enum Couleur {Rouge = 0, Vert, Jaune, Bleu, Orange, Blanc};
+void appuiRouge(){
+  if(Mode == NULL){
+    Mode = Config;
+  }
+}
 
 // Intitialisation du programme
 void setup() 
 {
   Serial.begin(9600);
-  ss.begin(9600);
   Serial.println("Demarrage du programme");
-  SD.begin(pinCS);
   pinMode(boutonRouge, INPUT);
   pinMode(boutonVert, INPUT);
-  pinMode(pinRouge, OUTPUT);
-  pinMode(pinVert, OUTPUT);
-  pinMode(pinBleu, OUTPUT);
-  time.begin();
 
   //interruption possible sur le bouton rouge pour passer en mode config
-  Mode=Config;
-  attachInterrupt(digitalPinToInterrupt(boutonRouge), modeConfig, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(boutonRouge), appuiRouge, CHANGE);
   delay(10000);
-  Mode=Standard;
-}
-
-// Fonction permettant de basculer d'un mode à l'autre
-void Modes(Mode) 
-{
-  // Mode Standard
-  if(Mode == Standard)
-  {
-    modeStandard();
+  if(Mode == NULL){
+    Mode = Standard;
   }
-  // Mode Eco   
-  else if(Mode == Eco)
-  {
-    modeEco();
+}
+
+void loop(){
+  if(Mode == Standard){
+    Serial.println("Stand");
   }
-  // Mode Maintenance
-  else if(Mode == Maintenance)
-  {
-    modeMaintenance();
+  else if(Mode == Config){
+    Serial.println("Conf");
   }
-  // Mode Config
-  else if(Mode == Config)
-  {
-    modeConfig();
-  }
-  // Gestion des erreurs : si pas de mode, renvoie 0
-  return 0;
 }
 
-// Fonction du mode Standard
-void modeStandard()
-{  
-  // Initialisation des interruptions
-  attachinterrupt(digitalPinToInterrupt(boutonRouge), appuiBoutonRougeS, CHANGE);
-  attachinterrupt(digitalPinToInterrupt(boutonVert), appuiBoutonVertS, CHANGE);
-  // Mesure et sauvegarde des capteurs avec vérification des erreurs 
-  mesureCapteurs();
-  sauvMesure();
-  verifErreurs();
-  return Mode;
-}
-
-// Fonction mode Eco
-void modeEco()
-{ 
-  // Initialisation des interruptions
-  attachinterrupt(digitalPinToInterrupt(boutonRouge), appuiBoutonRougeE, CHANGE);
-  attachinterrupt(digitalPinToInterrupt(boutonVert), appuiBoutonVertE, CHANGE);
-  // Mesure et sauvegarde des capteurs avec vérification des erreurs 
-  mesureCapteurs();
-  sauvMesure();
-  checkErreur();
-  return Mode;
-}
-
-// Fonction mode Maintenance
-void modeMaintenance() 
-{
-  // Initialisation des interruptions
-  attachinterrupt(digitalPinToInterrupt(boutonRouge), appuiBoutonRougeM, CHANGE);
-  // Mise à jour du clignottement des LEDs
-  couleurLed(Orange)
-  // Affichage du mode
-  Serial.println("Mode Maintenance");
-  stopMesure();
-  accesSD();
-  affSerie();
-  return Mode;
-}
-
-void sauvMesure()
-{
-  //Initialiser une variable qui compte le nombre de fichiers dans un dossier
-  int nbFichiers = 0;
-
-  //Ouvrir un dossier
-  File dir = SD.open("/");
-
-     //Si le dossier contient 9 fichiers
-      if (nbFichiers == 9){
-        //Créer un nouveau dossier
-        File newDir = SD.open("/nouveauDossier");
-        //Réinitialiser la variable qui compte le nombre de fichiers dans un dossier
-        nbFichiers=0;
-      }
-      //ouvrir le fichier
-      File dataFile = SD.open("data.txt", FILE_WRITE);
-      //si le fichier s'ouvre correctement
-      if (dataFile) {
-        //écrire les mesures dans le fichier
-        dataFile.println(MesuresCapteurs);
-        //incrémenter la variable qui compte le nombre de fichiers dans un dossier
-        nbFichiers++;
-        //fermer le fichier
-        dataFile.close();
-      }
-      else{
-        //si le fichier ne s'ouvre pas correctement
-        Serial.println("erreur d'ouverture du fichier");
-      }
-}
-
-
-void loop() 
-{
-  Modes();
-}
