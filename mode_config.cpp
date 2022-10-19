e7732248029f0c1d599e523aeb7753736b6da16b
ArnaudWierel <arnaud.67210@gmail.com>
2022-10-19 09:22:34 +0200

    YESSS ÇA MARCHE

--- mode_config.cpp
+++ mode_config.cpp
@@ -5,7 +5,9 @@
 //#include "mode.h"
 //#include <iostream>
 
-String c; // variable pour stocker la commande entrée par l’utilisateur
+String commandeEnter; // variable pour stocker la commande entrée par l’utilisateur
+String valeurEnter;   // variable pour stocker la valeur entrée par l’utilisateur
+int valeurEnterInt;   // variable pour stocker la valeur entrée par l’utilisateur en int
 
 // Initialisation du mode
 void setup()
@@ -15,120 +17,136 @@ void setup()
     Serial.println("Entrez une commande"); // Affichage d’un message d’information
 }
 
-// Boucle de traitement du mode
 void loop()
 {
-    if (Serial.available() > 0) // Si les données sont disponibles sur la liaison série
+    Commande(); // Appel de la fonction Commande
+}
+
+// Boucle de traitement du mode
+void Commande()
+{
+    while (Serial.available() == 0)
     {
-        while (c == "") // Atendre l'entré de l'utilisateur
+    }                                           // wait for data available
+    String commandeEnter = Serial.readString(); // read until timeout
+    commandeEnter.trim();
+    if (commandeEnter == "LOG_INTERVAL")
+    {
+        Serial.println("LOG_INTERVAL");
+        Serial.println("Rentrez la nouvelle valeur pour LOG_INTERVAL");
+        while (Serial.available() == 0)
         {
-            delay(10);
-            c = Serial.readString();
         }
+        valeurEnter = Serial.readString();
+        if (valeurEnter != "")
+        {
+            valeurEnterInt = valeurEnter.toInt();
+            Serial.print("LOG_INTERVAL mis à jour à la valeur ");
+            Serial.println(valeurEnterInt);
+            setup();
+        }
+    }
 
-        if (c == "LOG_INTERVAL")
-        {
-            Serial.println("LOG_INTERVAL");
-        }
+    else if (commandeEnter == "FILE_MAX_SIZE")
+    {
+        Serial.println("FILE_MAX_SIZE");
+    }
 
-        else if (c == "FILE_MAX_SIZE")
-        {
-            Serial.println("FILE_MAX_SIZE");
-        }
+    else if (commandeEnter == "RESET")
+    {
+        Serial.println("RESET");
+    }
 
-        else if (c == "RESET")
-        {
-            Serial.println("RESET");
-        }
+    else if (commandeEnter == "VERSION")
+    {
+        Serial.println("VERSION");
+    }
 
-        else if (c == "VERSION")
-        {
-            Serial.println("VERSION");
-        }
+    else if (commandeEnter == "TIMEOUT")
+    {
+        Serial.println("TIMEOUT");
+    }
 
-        else if (c == "TIMEOUT")
-        {
-            Serial.println("TIMEOUT");
-        }
+    else if (commandeEnter == "LUMIN_LOW")
+    {
+        Serial.println("LUMIN_LOW");
+    }
 
-        else if (c == "LUMIN_LOW")
-        {
-            Serial.println("LUMIN_LOW");
-        }
+    else if (commandeEnter == "LUMIN_HIGH")
+    {
+        Serial.println("LUMIN_HIGH");
+    }
 
-        else if (c == "LUMIN_HIGH")
-        {
-            Serial.println("LUMIN_HIGH");
-        }
+    else if (commandeEnter == "TEMP_AIR")
+    {
+        Serial.println("TEMP_AIR");
+    }
 
-        else if (c == "TEMP_AIR")
-        {
-            Serial.println("TEMP_AIR");
-        }
+    else if (commandeEnter == "MIN_TEMP_AIR")
+    {
+        Serial.println("MIN_TEMP_AIR");
+    }
 
-        else if (c == "MIN_TEMP_AIR")
-        {
-            Serial.println("MIN_TEMP_AIR");
-        }
+    else if (commandeEnter == "MAX_TEMP_AIR")
+    {
+        Serial.println("MAX_TEMP_AIR");
+    }
 
-        else if (c == "MAX_TEMP_AIR")
-        {
-            Serial.println("MAX_TEMP_AIR");
-        }
+    else if (commandeEnter == "HYGR")
+    {
+        Serial.println("HYGR");
+    }
 
-        else if (c == "HYGR")
-        {
-            Serial.println("HYGR");
-        }
+    else if (commandeEnter == "HYGR_MINT")
+    {
+        Serial.println("HYGR_MINT");
+    }
 
-        else if (c == "HYGR_MINT")
-        {
-            Serial.println("HYGR_MINT");
-        }
+    else if (commandeEnter == "HYGR_MAXT")
+    {
+        Serial.println("HYGR_MAXT");
+    }
 
-        else if (c == "HYGR_MAXT")
-        {
-            Serial.println("HYGR_MAXT");
-        }
+    else if (commandeEnter == "PRESSURE")
+    {
+        Serial.println("PRESSURE");
+    }
 
-        else if (c == "PRESSURE")
-        {
-            Serial.println("PRESSURE");
-        }
+    else if (commandeEnter == "PRESSURE_MIN")
+    {
+        Serial.println("PRESSURE_MIN");
+    }
 
-        else if (c == "PRESSURE_MIN")
-        {
-            Serial.println("PRESSURE_MIN");
-        }
+    else if (commandeEnter == "PRESSURE_MAX")
+    {
+        Serial.println("PRESSURE_MAX");
+    }
 
-        else if (c == "PRESSURE_MAX")
-        {
-            Serial.println("PRESSURE_MAX");
-        }
+    else if (commandeEnter == "CLOCK")
+    {
+        Serial.println("CLOCK");
+    }
 
-        else if (c == "CLOCK")
-        {
-            Serial.println("CLOCK");
-        }
+    else if (commandeEnter == "DATE")
+    {
+        Serial.println("DATE");
+    }
 
-        else if (c == "DATE")
-        {
-            Serial.println("DATE");
-        }
+    else if (commandeEnter == "DAY")
+    {
+        Serial.println("DAY");
+    }
 
-        else if (c == "DAY")
-        {
-            Serial.println("DAY");
-        }
+    else if (commandeEnter == "HELP")
+    {
+        Serial.println("HELP");
+    }
 
-        else if (c == "HELP")
-        {
-            Serial.println("HELP");
-        }
-
-        else
-        {
-            Serial.println("Commande inconnue");
-        }
+    else
+    {
+        Serial.print("Commande ");
+        Serial.print(commandeEnter);
+        Serial.print(" inconnue \n");
+        setup();
     }
 }
