2111ee6d6f9c5be36ff6d35d29a2d973217d32a0
EikyouPH <91479888+EikyouPH@users.noreply.github.com>
2022-10-19 13:46:51 +0200

    Formatage

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@
 #include "DHT.h"
 
 ChainableLED leds(6, 7, 1);
-enum mode {Standard = 0, Eco, Maintenance, Config, Debut};
+enum mode
+{
+  Standard = 0,
+  Eco,
+  Maintenance,
+  Config,
+  Debut
+};
 const int boutonRouge = 2;
 const int boutonVert = 3;
 // Pins pour les capteurs
@@ -20,25 +27,34 @@ int tmp;
 #define DHTPIN 8
 DHT dht(DHTPIN, DHTTYPE);
 
-void appuiRouge(){
+void appuiRouge()
+{
   bRouge = digitalRead(boutonRouge);
-  if(!bRouge){
+  if (!bRouge)
+  {
     duree = millis();
   }
-  else if(bRouge){
-    if(millis()-duree > 5000){
-      if(Mode == Debut){
+  else if (bRouge)
+  {
+    if (millis() - duree > 5000)
+    {
+      if (Mode == Debut)
+      {
         Mode = Config;
       }
-      else if(Mode == Standard || Mode == Eco){
+      else if (Mode == Standard || Mode == Eco)
+      {
         precMode = Mode;
         Mode = Maintenance;
       }
-      else if(Mode == Maintenance){
-        if(precMode == Eco){
+      else if (Mode == Maintenance)
+      {
+        if (precMode == Eco)
+        {
           Mode = Eco;
         }
-        else{
+        else
+        {
           Mode = Standard;
         }
       }
@@ -46,18 +62,23 @@ void appuiRouge(){
   }
 }
 
-
-void appuiVert(){
+void appuiVert()
+{
   bVert = digitalRead(boutonVert);
-  if(!bVert){
+  if (!bVert)
+  {
     duree = millis();
   }
-  else if(bVert){
-    if(millis()-duree > 5000){
-      if(Mode == Standard){
+  else if (bVert)
+  {
+    if (millis() - duree > 5000)
+    {
+      if (Mode == Standard)
+      {
         Mode = Eco;
       }
-      else if(Mode == Eco){
+      else if (Mode == Eco)
+      {
         Mode = Standard;
       }
     }
@@ -65,7 +86,7 @@ void appuiVert(){
 }
 
 // Intitialisation du programme
-void setup() 
+void setup()
 {
   Mode = Debut;
   Serial.begin(9600);
@@ -79,33 +100,34 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(boutonRouge), appuiRouge, CHANGE);
   attachInterrupt(digitalPinToInterrupt(boutonVert), appuiVert, CHANGE);
 
-  //interruption possible sur le bouton rouge pour passer en mode config
+  // interruption possible sur le bouton rouge pour passer en mode config
   delay(5000);
-  if(Mode == Debut){
+  if (Mode == Debut)
+  {
     Mode = Standard;
   }
 }
 
 // Fonction permettant de basculer d'un mode à l'autre
-void Modes() 
+void Modes()
 {
   // Mode Standard
-  if(Mode == Standard)
+  if (Mode == Standard)
   {
     modeStandard();
   }
-  // Mode Eco   
-  else if(Mode == Eco)
+  // Mode Eco
+  else if (Mode == Eco)
   {
     modeEco();
   }
   // Mode Maintenance
-  else if(Mode == Maintenance)
+  else if (Mode == Maintenance)
   {
     modeMaintenance();
   }
   // Mode Config
-  else if(Mode == Config)
+  else if (Mode == Config)
   {
     modeConfig();
   }
@@ -113,27 +135,32 @@ void Modes()
   return 0;
 }
 
-void loop(){
+void loop()
+{
   Modes();
 }
 
-void modeStandard(){
+void modeStandard()
+{
   Serial.println("Mode standard");
   delay(10);
   leds.setColorRGB(0, 0, 255, 0);
   mesureCapteurs();
 }
 
-void modeEco(){
+void modeEco()
+{
   Serial.println("Eco");
   delay(10);
   leds.setColorRGB(0, 0, 0, 255);
 }
-void modeConfig(){
+void modeConfig()
+{
   Serial.println("Conf");
   leds.setColorRGB(0, 255, 255, 0);
 }
-void modeMaintenance(){
+void modeMaintenance()
+{
   Serial.println("Maintenance");
   delay(10);
   leds.setColorRGB(0, 255, 127, 0);
@@ -141,7 +168,8 @@ void modeMaintenance(){
   delay(3000);
 }
 
-void affichage(float lumiere, float humidite, float temperature, float pression){
+void affichage(float lumiere, float humidite, float temperature, float pression)
+{
   Serial.print("Niveau luminosite : ");
   Serial.print(lumiere);
   Serial.println(" lux");
@@ -156,7 +184,6 @@ void affichage(float lumiere, float humidite, float temperature, float pression)
   Serial.println(" Pa");
 }
 
-
 float mesureCapteurs()
 {
   float lumiere = analogRead(pinLux);
@@ -167,33 +194,41 @@ float mesureCapteurs()
   float temperature = temp_hum_val[1];
   float pression = 8.3144621 * (273.15 + temperature) * masseVolumique(temperature) / 0.029;
   erreur(lumiere, humidite, temperature, pression);
-  if(Mode == Maintenance){
+  if (Mode == Maintenance)
+  {
     affichage(lumiere, humidite, temperature, pression);
   }
   return lumiere, humidite, temperature, pression;
 }
 
- float masseVolumique(float temperature){
-   if(temperature < 7.5){
-     return 1.292;
-   }
-    else if(7.5 <= temperature < 17.5){
-     return 1.225 ;
-   }
-    else if(17.5 <= temperature < 22.5){
-     return 1.204 ;
-   }
-    else if(22.5 <= temperature){
-     return 1.292 ;
-   }
- }
+float masseVolumique(float temperature)
+{
+  if (temperature < 7.5)
+  {
+    return 1.292;
+  }
+  else if (7.5 <= temperature < 17.5)
+  {
+    return 1.225;
+  }
+  else if (17.5 <= temperature < 22.5)
+  {
+    return 1.204;
+  }
+  else if (22.5 <= temperature)
+  {
+    return 1.292;
+  }
+}
 
-void erreur(float lumiere, float humidite, float temperature, float pression){
+void erreur(float lumiere, float humidite, float temperature, float pression)
+{
   // horloge 0
   // GPS 1
   // capteur inaccessible 2
-  //données incoherentes 3
-  if(lumiere < 0 || lumiere > 1000 || humidite < 0 || humidite > 100 || temperature < -50 || temperature > 50 || pression < 100000 || pression > 110000){
+  // données incoherentes 3
+  if (lumiere < 0 || lumiere > 1000 || humidite < 0 || humidite > 100 || temperature < -50 || temperature > 50 || pression < 100000 || pression > 110000)
+  {
     clignotement(3);
   }
   // carte sd pleine 4
@@ -201,46 +236,48 @@ void erreur(float lumiere, float humidite, float temperature, float pression){
   // passage au mode précédent
 }
 
-void clignotement(int type){
-  while(true){
-    switch(type)
+void clignotement(int type)
+{
+  while (true)
+  {
+    switch (type)
     {
-      case 0: // horloge
-        leds.setColorRGB(0, 255, 0, 0);
-        delay(1000);
-        leds.setColorRGB(0, 0, 0, 255);
-        delay(1000);
-        break;
-      case 1: // GPS
-        leds.setColorRGB(0, 255, 0, 0);
-        delay(1000);
-        leds.setColorRGB(0, 255, 255, 0);
-        delay(1000);
-        break;
-      case 2: // acces capteur
-        leds.setColorRGB(0, 255, 0, 0);
-        delay(1000);
-        leds.setColorRGB(0, 0, 255, 0);
-        delay(1000);
-        break;
-      case 3: // donnee incoherente
-        leds.setColorRGB(0, 255, 0, 0);
-        delay(1000);
-        leds.setColorRGB(0, 0, 255, 0);
-        delay(2000);
-        break;
-      case 4: // SD pleine
-        leds.setColorRGB(0, 255, 0, 0);
-        delay(1000);
-        leds.setColorRGB(0, 255, 255, 255);
-        delay(1000);
-        break;
-      case 5: // SD inaccessible
-        leds.setColorRGB(0, 255, 0, 0);
-        delay(1000);
-        leds.setColorRGB(0, 255, 255, 255);
-        delay(2000);
-        break;
+    case 0: // horloge
+      leds.setColorRGB(0, 255, 0, 0);
+      delay(1000);
+      leds.setColorRGB(0, 0, 0, 255);
+      delay(1000);
+      break;
+    case 1: // GPS
+      leds.setColorRGB(0, 255, 0, 0);
+      delay(1000);
+      leds.setColorRGB(0, 255, 255, 0);
+      delay(1000);
+      break;
+    case 2: // acces capteur
+      leds.setColorRGB(0, 255, 0, 0);
+      delay(1000);
+      leds.setColorRGB(0, 0, 255, 0);
+      delay(1000);
+      break;
+    case 3: // donnee incoherente
+      leds.setColorRGB(0, 255, 0, 0);
+      delay(1000);
+      leds.setColorRGB(0, 0, 255, 0);
+      delay(2000);
+      break;
+    case 4: // SD pleine
+      leds.setColorRGB(0, 255, 0, 0);
+      delay(1000);
+      leds.setColorRGB(0, 255, 255, 255);
+      delay(1000);
+      break;
+    case 5: // SD inaccessible
+      leds.setColorRGB(0, 255, 0, 0);
+      delay(1000);
+      leds.setColorRGB(0, 255, 255, 255);
+      delay(2000);
+      break;
     }
   }
 }
